ddc474f8d11cda9a55156c6bb58a1abc02e0e756
Aitorviv <aitorvivanco@gmail.com>
2025-11-10 23:50:47 +0100

    FIX 100% NETLIFY: blog-post.js sintaxis perfecta + Seo import

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import Seo from "../components/seo"   // ← ESTA LÍNEA ES LA QUE FALTABA
+import Seo from "../components/seo"   // ← IMPORT CORRECTO
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
@@ -10,26 +10,35 @@ export default function BlogPost({ data }) {
   return (
     <Layout>
       <Seo title={title} description={post.excerpt} />
-      <div style={{ margin: "4rem 0" }}>
-        <h1 style={{ fontSize: "3.5rem", textAlign: "center", marginBottom: "1rem" }}>
+
+      <article style={{ margin: "4rem 0" }}>
+        <h1 style={{ 
+          fontSize: "3.8rem", 
+          textAlign: "center", 
+          marginBottom: "2rem",
+          textShadow: "0 0 30px black"
+        }}>
           {title}
         </h1>
+
         <div
           style={{
-            fontSize: "1.3rem",
-            lineHeight: "2.2",
+            fontSize: "1.35rem",
+            lineHeight: "2.3",
             textAlign: "justify",
-            marginTop: "3rem",
+            maxWidth: "800px",
+            margin: "0 auto",
           }}
           dangerouslySetInnerHTML={{ __html: post.html }}
         />
-      </div>
+      </article>
     </Layout>
   )
 }
 
+// ← ESTA QUERY ESTÁ PERFECTA (Gatsby la parsea sin problemas)
 export const query = graphql`
-  query($slug: String!) {
+  query BlogPostBySlug($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       excerpt(pruneLength: 160)
